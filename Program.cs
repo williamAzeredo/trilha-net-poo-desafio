301eaa4b711317a8b4020067fc6bfbed0a8decd3
William Azeredo <dtwy@dev.azure.com>
2023-10-25 16:12:40 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste no IPHONE");
+Iphone ip = new Iphone("123","11","252368",8);
+ip.Ligar();
+ip.InstalarAplicativo("Instalando Aplicativo IPhone");
+ip.ReceberLigacao();
+Console.WriteLine("==============================");
+Console.WriteLine("Teste no NOKIA");
+Nokia nk = new Nokia("321", "N45", "363636", 4);
+nk.Ligar();
+nk.InstalarAplicativo("Instalando Aplicativo Nokia");
+nk.ReceberLigacao();
+Console.WriteLine("==============================");
